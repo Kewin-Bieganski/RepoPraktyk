9226148d25d1e347158be361d53e8293a22b5761
Kewin Biegański <kewinbieganski@gmail.com>
2022-07-02 23:55:59 +0200

    Dalsze tworzenie gry Ratuj Ludzi

--- RepoPraktyk/App.xaml.cs
+++ RepoPraktyk/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace RepoPraktyk
+namespace RatujLudzi
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
