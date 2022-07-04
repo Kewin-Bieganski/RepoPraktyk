13c6f42208788f406a1377110f100025dc7a2640
Kewin Biegański <kewinbieganski@gmail.com>
2022-07-04 22:07:46 +0200

    Ukończenie: win - dows forms

--- Win - dows forms/Program.cs
+++ Win - dows forms/Program.cs
@@ -6,7 +6,7 @@ namespace Win___dows_forms
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void NotMain()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
