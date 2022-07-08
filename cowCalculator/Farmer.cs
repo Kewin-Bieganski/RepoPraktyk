92eb06a2411642ca2ec0706eba65097759d7a75b
Kewin Biegański <kewinbieganski@gmail.com>
2022-07-08 10:35:41 +0200

    Ukoczenie: cowCalculator

---
+++ cowCalculator/Farmer.cs
@@ -0,0 +1,39 @@
+﻿namespace cowCalculator
+{
+    internal class Farmer
+    {
+        private int feedMultiplier;
+        public int FeedMultiplier { get { return feedMultiplier; } }
+        public int BagsOfFeed { get; private set; }
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+
+        public Farmer(int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+
+        public int GetNumberOfCows()
+        {
+            return numberOfCows;
+        }
+        public void SetNumberOfCows(int numberOfCows)
+        {
+            this.numberOfCows = numberOfCows;
+            BagsOfFeed = this.numberOfCows * FeedMultiplier;
+        }
+    }
+}
