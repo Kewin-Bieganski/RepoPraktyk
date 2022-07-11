75d9864eb3b10cb6799f35f74ffd2431306a16bb
Kewin Biegański <kewinbieganski@gmail.com>
2022-07-11 09:44:13 +0200

    Ukonczenie: zlodziejKlejnotow

---
+++ zlodziejKlejnotow/Locksmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zlodziejKlejnotow
+{
+    internal class Locksmith
+    {
+        public void OpenSafe(Safe safe, Owner owner)
+        {
+            safe.PickLock(this);
+            Jewels safeContetns = safe.Open(writtenDownCombination);
+            ReturnContents(safeContetns, owner);
+        }
+
+        private string writtenDownCombination = null;
+        public void WriteDownCombination(string combination)
+        {
+            writtenDownCombination = combination;
+        }
+        
+        public virtual void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
+    }
+}
---
+++ zlodziejKlejnotow/Safe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zlodziejKlejnotow
+{
+    internal class Safe
+    {
+        private Jewels contents = new Jewels();
+        private string safeCombination = "12345";
+        public Jewels Open(string combination)
+        {
+            return (combination.Equals(safeCombination) ? contents : null);
+        }
+
+        public void PickLock(Locksmith lockpicker)
+        {
+            lockpicker.WriteDownCombination(safeCombination);
+        }
+    }
+}
