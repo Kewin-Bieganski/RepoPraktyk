16c45119a678f1619cecaf8aeb7d950bea2730ba
Kewin Biegański <kewinbieganski@gmail.com>
2022-07-13 23:31:33 +0200

    Ukonczenie: shoes

---
+++ shoes/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace shoes
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Shoe> shoeCloset = new List<Shoe>()
+            {
+                new Shoe()
+                {
+                    Style = Style.Sneakers,
+                    Color = "Czarny",
+                },
+                new Shoe()
+                {
+                    Style = Style.Clogs,
+                    Color = "Brązowy",
+                },
+                new Shoe()
+                {
+                    Style = Style.Wingtips,
+                    Color = "Czarny",
+                },
+                new Shoe()
+                {
+                    Style = Style.Loafers,
+                    Color = "Biały",
+                },
+                new Shoe()
+                {
+                    Style = Style.Loafers,
+                    Color = "Czerwony",
+                },
+                new Shoe()
+                {
+                    Style = Style.Sneakers,
+                    Color = "Zielony",
+                },
+            };
+
+            int numberOfShoes = shoeCloset.Count;
+            foreach (Shoe shoe in shoeCloset)
+            {
+                shoe.Style = Style.Flipflops;
+                shoe.Color = "Pomarańczowy";
+            }
+
+            Shoe thirdShoe = shoeCloset[2];
+            Shoe secondShoe = shoeCloset[1];
+            shoeCloset.Clear();
+            
+            shoeCloset.Add(thirdShoe);
+            shoeCloset.Add(secondShoe);
+            if(shoeCloset.Contains(secondShoe))
+            {
+                Console.WriteLine("A to ci niespodzianka");
+            }
+        }
+    }
+
+    public class Shoe
+    {
+        public Style Style;
+        public string Color;
+    }
+
+    public enum Style
+    {
+        Sneakers,
+        Loafers,
+        Sandals,
+        Flipflops,
+        Wingtips,
+        Clogs,
+    }
+}
