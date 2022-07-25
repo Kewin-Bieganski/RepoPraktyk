176a52eb991534753a99bdb6f239379da8b97123
Kewin Biegański <kewinbieganski@gmail.com>
2022-07-25 10:16:10 +0200

    Ukonczenie: hive-manager-xaml

---
+++ hiveManagerXaml/INectarCollector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hiveManager
+{
+    internal interface INectarCollector
+    {
+        void FindFlowers();
+        void GatherNectar();
+        void ReturnToHive();
+    }
+}
---
+++ hiveManagerXaml/Queen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace hiveManager
+{
+    internal class Queen : Bee
+    {
+        private Worker[] workers;
+        private int shiftNumber = 0;
+
+        public Queen(Worker[] workers, double weightMg) : base(weightMg)
+        {
+            this.workers = workers;
+        }
+
+        public bool AssingWork(string job, int numberOfShifts)
+        {
+            for (int i = 0; i < workers.Length; i++)
+            {
+                if (workers[i].DoThisJob(job, numberOfShifts))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public string WorkTheNextShift()
+        {
+            double honeyConsumed = HoneyConsumptionRate();
+            shiftNumber++;
+            string report = $"Raport zmiany numer {shiftNumber}\r\n";
+            for (int i = 0; i < workers.Length; i++)
+            {
+                honeyConsumed += workers[i].HoneyConsumptionRate();
+                if (workers[i].DidYouFinish())
+                {
+                    report += $"Robotnica numer {(i + 1)} zakończyła swoje zadanie \r\n";
+                }
+                if (String.IsNullOrEmpty(workers[i].CurrentJob))
+                {
+                    report += $"Robotnica numer {(i + 1)} nie pracuje \r\n";
+                } else
+                {
+                    if (workers[i].ShiftsLeft > 0)
+                    {
+                        report += $"Robotnica numer {(i + 1)} robi '{workers[i].CurrentJob}' po tej zmiamie\r\n";
+                    }
+                }
+            }
+            report += $"Całkowite spożycie miodu: {honeyConsumed} jednostek\r\n";
+            return report;
+        }
+    }
+}
