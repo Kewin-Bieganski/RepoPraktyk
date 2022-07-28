3f6549c2d2dd2a42adc89575e0839b1f8c58dbcd
Kewin Biegański <kewinbieganski@gmail.com>
2022-07-28 14:52:29 +0200

    Ukonczenie: serializeGuyXamlWithGui

---
+++ serializeGuyXamlWithGui/GuyManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.Serialization;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace serializeGuyXamlWithGui
+{
+    internal class GuyManager : INotifyPropertyChanged
+    {
+        private IStorageFile latestGuyFile;
+        private Guy joe = new Guy("Joe", 37, 123M);
+        private Guy bob = new Guy("Bob", 45, 4.23M);
+        private Guy ed = new Guy("Ed", 43, 39.3M);
+
+        public IStorageFile LatestGuyFile { get { return latestGuyFile; } }
+        public Guy Joe { get { return joe; } }
+        public Guy Bob { get { return bob; } }
+        public Guy Ed { get { return ed; } }
+        public Guy NewGuy { get; private set; }
+        public string Path { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public async void ReadGuyAsync()
+        {
+            if (String.IsNullOrEmpty(Path)) return;
+            latestGuyFile = await StorageFile.GetFileFromPathAsync(Path);
+            using (IRandomAccessStream stream = await latestGuyFile.OpenAsync(FileAccessMode.Read))
+            using (Stream inputStream = stream.AsStreamForRead())
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Guy));
+                NewGuy = serializer.ReadObject(inputStream) as Guy;
+                OnPropertyChanged("NewGuy");
+                OnPropertyChanged("LatestGuyFile");
+            }
+        }
+
+        public async void WriteGuyAsync(Guy guyToWrite)
+        {
+            IStorageFolder guysFolder = await KnownFolders.DocumentsLibrary.CreateFolderAsync("Faceci", CreationCollisionOption.OpenIfExists);
+            latestGuyFile = await guysFolder.CreateFileAsync(guyToWrite.Name + ".xml", CreationCollisionOption.ReplaceExisting);
+            using (IRandomAccessStream stream = await latestGuyFile.OpenAsync(FileAccessMode.ReadWrite))
+            using (Stream outputStream = stream.AsStreamForWrite())
+            {
+                DataContractSerializer dataContractSerializer = new DataContractSerializer(typeof(Guy));
+                dataContractSerializer.WriteObject(outputStream, guyToWrite);
+            }
+            Path = latestGuyFile.Path;
+            OnPropertyChanged("Path");
+            OnPropertyChanged("LatestGuyFile");
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChangedEventHandler = PropertyChanged;
+            if (propertyChangedEventHandler != null) propertyChangedEventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
